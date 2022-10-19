6814a2180cccd6e4d0f733abf48029720fc323bc
ishtos <ginkgonut.9960@gmail.com>
2022-10-19 20:16:11 +0900

    refactor: remove version number

--- pymessari/__init__.py
+++ pymessari/__init__.py
@@ -4,7 +4,6 @@
 @File    :   __init__.py
 @Time    :   2022/10/19 14:50:33
 @Author  :   Next Finance Tech
-@Version :   0.0.1
 @License :   (C)Copyright 2022 Next Finance Tech
 """
 
--- pymessari/pymessari.py
+++ pymessari/pymessari.py
@@ -4,7 +4,6 @@
 @File    :   pymessari.py
 @Time    :   2022/10/19 14:50:19
 @Author  :   Next Finance Tech
-@Version :   0.0.1
 @License :   (C)Copyright 2022 Next Finance Tech
 """
 
