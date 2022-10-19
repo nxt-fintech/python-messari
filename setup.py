2148fd446fd128f3da2d624431ca160fcee3cffc
ishtos <ginkgonut.9960@gmail.com>
2022-10-19 19:35:58 +0900

    feature: setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+requires = ["requests"]
+
+setup(
+    name="pymessari",
+    version="0.0.1",
+    url="https://https://github.com/nxt-fintech/pymessari",
+    description="client library for messari api",
+    author="Next Finance Tech",
+    license="APACHE LICENSE, VERSION 2.0¶",
+    packages=["pymessari"],
+    install_requires=requires,
+    classifiers=["Programming Language :: Python"],
+    keywords=["messari"],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
