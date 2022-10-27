f2a114b1fb4468960b22917f70022e78943c753c
ishtos <ginkgonut.9960@gmail.com>
2022-10-28 01:40:00 +0900

    release: v0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ requires = ["requests"]
 
 setup(
     name="pymessari",
-    version="0.0.7",
+    version="0.1.0",
     url="https://github.com/nxt-fintech/pymessari",
     description="client library for messari api",
     author="Next Finance Tech",
